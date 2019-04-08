ecd1998ad679bedc3e1c000665915ad15f6941bf
iamatran <iamatran@hotmail.com>
2019-04-07 22:59:18 -0500

    1

---
+++ groceries/src/pages/home/home.ts
@@ -0,0 +1,144 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery";
+
+  items = [
+      {
+          name: "Milk",
+          quantity: 2
+      },
+      {
+          name: "Bread",
+          quantity: 1
+      },
+      {
+          name: "Banana",
+          quantity: 3
+      },
+      {
+          name: "Sugar",
+          quantity: 1
+      },
+
+  ];
+
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+        message: "Removing Item - " + item.name + " ..." ,
+        duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1);
+
+  }
+
+  editItem(item, index) {
+    console.log("Editinging Item - ", item, index);
+    const toast = this.toastCtrl.create({
+        message: "Editing Item - " + item.name + " ..." ,
+        duration: 3000
+    });
+    toast.present();
+    this.showEditItemPrompt(item, index);
+
+  }
+
+
+
+  addItem() {
+    console.log('meow');
+    this.showAddItemPrompt();
+  }
+
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+
+  showEditItemPrompt(item, index) {
+    const prompt = this.alertCtrl.create({
+      title: 'Edit Item',
+      message: "Please edit item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+          value: item.name
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity',
+          value: item.quantity
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            //this.items.push(item);
+            this.items[index] = item;
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+
+
+
+}
